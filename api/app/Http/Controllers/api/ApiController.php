09fd593697334649ce97fe54fca8d6baadf814d7
sarangan2209 <sarangan@tecofize.com>
2025-03-06 11:16:24 +0530

    update

--- api/app/Http/Controllers/api/ApiController.php
+++ api/app/Http/Controllers/api/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
@@ -46,4 +48,44 @@ class ApiController extends Controller
             'token' => $user->createToken('API TOKEN')->plainTextToken   
      ],200); 
     }
+
+    public function login(Request $request)
+    {
+
+        $validateuser = validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]
+        ); 
+
+        if($validateuser->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validateuser->errors()
+            ],401);
+
+        }
+
+
+        if(!Auth::attempt(($request->only(['email','password'])))){
+            return response()->json([
+                'status' => false,
+                'message' => 'something went wrong',
+            ],401); 
+
+        }    
+
+        $user = User::where('email', $request->email)->first();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'successfully login',
+            'token' => $user->createToken('API TOKEN')->plainTextToken
+        ],200);
+
+    
+}
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ApiController;
 
 Route::post('register', [ApiController::class, 'register']);
-
+Route::post('login', [Apicontroller::class, 'login'] );
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
